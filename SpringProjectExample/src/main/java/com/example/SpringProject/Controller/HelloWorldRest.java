ad72174eb3ab8e163032535b65ee75c85bc80f9d
Jayabharathi <Jayabharathi@Jayabharathi-PC>
2021-04-08 06:41:13 -0500

    code update

---
+++ SpringProjectExample/src/main/java/com/example/SpringProject/Controller/HelloWorldRest.java
@@ -0,0 +1,14 @@
+package com.example.SpringProject.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldRest {
+	
+@GetMapping(path="/Hello-World")
+public String HelloWorldMe() {
+	return "Hello World";
+}
+
+}
