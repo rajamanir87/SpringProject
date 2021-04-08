aa29f341ade9993becda4929476c665c009173f3
Jayabharathi <Jayabharathi@Jayabharathi-PC>
2021-04-08 06:02:35 -0500

    Spring Project updated

---
+++ SpringProjectExample/src/test/java/com/example/SpringProject/SpringProjectExample/SpringProjectExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringProject.SpringProjectExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
